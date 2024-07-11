54b96e4c2a3760632388cf4d6ff3608cba76f7b0
Chandan Kumar <128698211+kmrchandan006@users.noreply.github.com>
2024-07-11 19:38:13 +0530

    Start

---
+++ add.js
@@ -0,0 +1,6 @@
+// console.log("Chandan Kumar")
+const add = (a,b) => {
+    return a+b;
+};
+ module.exports = add;
+ 
---
+++ index.js
@@ -0,0 +1,6 @@
+ const addFn = require("./add");
+console.log("Chandan Kumar");
+ const sum = addFn(1,2);
+ const sum2 = addFn(2,3);
+  console.log(sum);
+  console.log(sum2);
